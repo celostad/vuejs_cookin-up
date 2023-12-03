ad6da4153af54f1d05162f18c79b646722b00fd5
celostad <celostad@gmail.com>
2023-12-02 23:56:43 -0300

    Etapa 03.Passando informações com props

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,36 +1,38 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index'
 import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CarCategoria.vue';
 
 export default {
-    data() {
-        return{
-            categorias: [] as ICategoria[]
-        }
-    },
-    async created(){
-      this.categorias = await obterCategorias();
+  data() {
+    return {
+      categorias: [] as ICategoria[]
     }
+  },
+  async created() {
+    this.categorias = await obterCategorias();
+  },
+  components: { CardCategoria }
 }
 </script>
 
 <template>
-    <section class="selecionar-ingredientes">
-        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
 
-        <p class="paragrafo-lg instrucoes">
-            Selecione abaixo os ingredientes que você quer usar nesta receita:
-        </p>
-        <ul class="categorias">
-            <li v-for="categoria in categorias" :key="categoria.nome">
-                {{ categoria.nome }}
-            </li>
-        </ul>
+    <p class="paragrafo-lg instrucoes">
+      Selecione abaixo os ingredientes que você quer usar nesta receita:
+    </p>
+    <ul class="categorias">
+      <li v-for="categoria in categorias" :key="categoria.nome">
+        <CardCategoria :categoria="categoria" />
+      </li>
+    </ul>
 
-        <p class="paragrafo dica">
-            *Atenção: consideramos que você tem em casa sal, pimenta e água.
-        </p>
-    </section>
+    <p class="paragrafo dica">
+      *Atenção: consideramos que você tem em casa sal, pimenta e água.
+    </p>
+  </section>
 </template>
 
 <style scope>
@@ -68,5 +70,4 @@ export default {
     margin-bottom: 2.5rem;
   }
 }
-
 </style>
